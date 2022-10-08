111e31746e6d6510a55920bc997fbe22b6d3c74f
fel <felicien.dehertogh@protonmail.com>
2022-10-08 17:40:34 +0200

    update button component

--- client/src/components/pages/Home/HomeLayout.tsx
+++ client/src/components/pages/Home/HomeLayout.tsx
@@ -1,11 +1,18 @@
 import { Grid, Paper } from '@mui/material'
 import ImageCard from '../../UI/ImageCard'
-import LogoutButton from '../../UI/Button/LogoutButton'
+import Button from '../../UI/Button/Button'
+import { useCookies } from 'react-cookie'
 
 export const HomeLayout = () => {
+    const [cookies, setCookie, removeCookie] = useCookies(['userToken'])
+
+    const handleLogout = () => {
+        removeCookie('userToken', { path: '/', sameSite: 'none', secure: true })
+    }
+
     return (
         <>
-            <LogoutButton />
+            <Button btnText="Logout" onClick={handleLogout} />
             <Grid container>
                 <Grid item container xs={6} md={4} lg={3}>
                     <Grid item m={0.5}>
