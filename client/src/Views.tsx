e89fd3a01f8b3395d3758f7e8dd226b115c5cb13
fel <felicien.dehertogh@protonmail.com>
2022-10-13 11:43:58 +0200

    remove Test component import from Views

--- client/src/Views.tsx
+++ client/src/Views.tsx
@@ -4,7 +4,6 @@ import { Register } from './components/pages/Register'
 import { NotFound } from './components/pages/NotFound'
 import { useCookies } from 'react-cookie'
 import { HomeLayout } from './components/pages/Home/HomeLayout'
-import { Test } from './components/pages/Home/Test'
 
 export const Views = () => {
     const [cookies, setCookie] = useCookies<string>(['userToken'])
